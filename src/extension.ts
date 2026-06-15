a0916f1bba9f9db92c3b31ea87e4cc9b95ac17fe
Alex Popkov <poalrom@toloka.ai>
2026-06-15 19:23:15 +0200

    chore: scaffold VSCode extension (esbuild + vitest)

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(_context: vscode.ExtensionContext): void {
+  console.log('Local Code Review activated');
+}
+
+export function deactivate(): void {}
